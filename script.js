6d91cc9b81cdd6cb29992f2779cd3429236e85c8
alfiandirizki <alfian.jw@gmail.com>
2023-06-15 22:37:55 +0700

    Change Init

---
+++ script.js
@@ -0,0 +1,23 @@
+    document.querySelector('#push').onclick = function(){
+            if(document.querySelector('#newtask input').value.length == 0 ){
+            alert("Please Enter a Task")
+        }   
+        else{document.querySelector('#tasks').innerHTML += `
+                    <div class="task">
+                        <span id="taskname">
+                            ${document.querySelector('#newtask input').value}
+                        </span>
+                        <button class="delete">
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                        </button>
+                     </div>
+        `;
+
+        var current_task = document.querySelectorAll(".delete");
+        for(var i=0; i<current_task.length; i++){
+            current_task[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+    }
